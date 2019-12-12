0fadf6e4e67221b016d11c730889d1618ef82aa4
Mustafa ESSOUFI <mustafa.essoufi@gmail.com>
2019-12-12 14:57:47 +0100

    first commit

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+  require_once './src/RandomQuotes.php';
+  // Creates a new object of RandomQuotes class.
+  $rq = new \RandomQuotes\RandomQuotes();
+  // Generates a random quote.
+  print_r( $rq->generate() );
+  echo "\n";
